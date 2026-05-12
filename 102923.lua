4e1c163110ebdaaf2a6835c9b8e513f6572af192
takgoomain-art <takgoo.main@gmail.com>
2026-05-12 23:17:21 +0800

    Update 102923.lua

--- 102923.lua
+++ 102923.lua
@@ -3356,7 +3356,7 @@ local iy = fe:Button({
 			loadstring(game:HttpGet('https://cdn.robloxscripts.gg/public/furky/furky-infinite-yield-roblox-admin-script-source.lua'))()
 		end})
 
-local exe = Exec:Section({
+local exe = More:Section({
 		Title = "Execute Script",
 		Desc = "Execute a loadstring script inside our script",
 		Icon = "monitor-cog",
@@ -3428,7 +3428,7 @@ local Button = exe:Button({
     end
 })
 
-local rf = Exec:Section({
+local rf = More:Section({
 		Title = "Remote Executor",
 		Desc = "Run codes inside our script.",
 		Icon = "chevrons-left-right-ellipsis",
